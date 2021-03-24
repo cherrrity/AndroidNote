583dbf16881af6ded447214ecf71dea657688879
cherrrity <hyej0126@gmail.com>
2021-03-25 01:51:20 +0900

    Image resources and compatibility

--- Kotlin Fundamentals/Anatomy of Basic Android Project/app/src/main/java/com/example/anatomyofbasicandroidproject/MainActivity.kt
+++ Kotlin Fundamentals/Anatomy of Basic Android Project/app/src/main/java/com/example/anatomyofbasicandroidproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.anatomyofbasicandroidproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -27,6 +28,19 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, "button Clicked!", Toast.LENGTH_SHORT).show()
         resultText.text = randomInt.toString()
+
+        val drawableResorce = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResorce)
+
     }
 
     private fun resetDice(){
